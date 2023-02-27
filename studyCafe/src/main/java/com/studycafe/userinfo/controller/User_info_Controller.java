2696d3876d8474cf93d623f998d56b74deb64ded
eungi <alsdmsrl02@naver.com>
2023-02-27 15:56:38 +0900

    1. db확인 및 xml문서 생성 -> src/main/resources -> mappers에서 생성 -> userinfo.xml생성 2. domain
    1. xml 생성 - db쿼리관련(src/main/resource)
    2. java파일 생성(domain)
    3. reposiroty생성
    4. controller생성
    5. service생성
    
    
    현재 : service먼저 수정중.
    수정완료되면 Controller 수정필요
    ->User_info_serviceImpl 회원목록 조회부분 수정필요
    controller생성 필요

---
+++ studyCafe/src/main/java/com/studycafe/userinfo/controller/User_info_Controller.java
@@ -0,0 +1,14 @@
+package com.studycafe.userinfo.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.studycafe.userinfo.service.User_info_Service;
+
+public class User_info_Controller {
+
+	private static final Logger logger = LoggerFactory.getLogger(User_info_Controller.class);
+	
+	private  User_info_Service User_info_Service; 
+	
+}//class의 끝
---
+++ studyCafe/src/main/java/com/studycafe/userinfo/service/User_info_ServiceImpl.java
@@ -0,0 +1,37 @@
+package com.studycafe.userinfo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.studycafe.userinfo.domain.User_info;
+import com.studycafe.userinfo.repository.User_info_repository;
+
+@Service
+public class User_info_ServiceImpl {
+
+	
+	@Autowired
+	private User_info_repository user_info_repository;
+	
+
+	//회원등록처리	
+	public void setNewUser_info(User_info User_info) {
+		user_info_repository.setNewUser_info(User_info);
+	}
+
+	//회원(id)로 상세회원조회
+	public User_info getUser_info_ById(String id) {
+		return user_info_repository.getUser_info_ById(id);
+	}
+	
+//	//회원목록조회
+//getAllUser_info_List()부분 오류 발생
+	/*
+	 * @Override public List<User_info> getAllUser_info_List(){ List<User_info>
+	 * user_info = user_info_repository.getAllUser_info_List(); return user_info; }
+	 */
+
+	
+}//class의 끝.
